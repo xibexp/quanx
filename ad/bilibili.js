1a76343c160886fcd6f846201c8c525ba4a8d0ac
xiao <xibexp@users.noreply.github.com>
2022-08-14 15:22:22 +0800

    Create bilibili.js

---
+++ ad/bilibili.js
@@ -0,0 +1,4 @@
+console.log("filter bilibili ad video");
+const body =  {...JSON.parse($response.body)};
+body.data.items = body.data.items.filter(item => item.card_goto !== "ad_av");
+$done({body:JSON.stringify(body)});

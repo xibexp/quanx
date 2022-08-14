7151788a4c6c8e9d311a121fd97f9203f765fe29
xiao <xibexp@users.noreply.github.com>
2022-08-14 15:34:36 +0800

    filter bilibili ad video

--- ad/bilibili.js
+++ ad/bilibili.js
@@ -1,4 +1,4 @@
 console.log("filter bilibili ad video");
 const body =  {...JSON.parse($response.body)};
-body.data.items = body.data.items.filter(item => item.card_goto !== "ad_av");
+body.data.items = body.data.items.filter(item => !item.card_goto.startsWith("ad"));
 $done({body:JSON.stringify(body)});
